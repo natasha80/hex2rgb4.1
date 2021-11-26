ec015fe3003dc2ef8f4021577dc41ee1f80968b5
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-11-26 07:10:38 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hex2rgb from "./components/Hex2rgb";
+
+function App() {
+  return (
+    <div className="App">
+      <Hex2rgb />
+    </div>
+  );
+}
+
+export default App;
